116ae3fac4793192e71c30fa598182a99a266fd1
Eric Clack <eric@bright-interactive.com>
2016-11-05 13:08:56 +0000

    More refactoring - seems to work now

--- news-page.js
+++ news-page.js
@@ -1,10 +1,41 @@
+const PRISMIC_API_URL = "http://clack.prismic.io/api";
+
+function getNewsItemsFromPrismic(callback) {
+    // Fire off *async* call to get news and then
+    // call callback with processed news items
+    Prismic.api(PRISMIC_API_URL, function(error, api) {
+        var options = {}; 
+        api.query(Prismic.Predicates.at('document.type', 'news-panel'), 
+                  options, function(error, response) { 
+                      if (error) {
+                          console.log("Prismic error: ", err);
+                      }
+                      else {
+                          callback(processPrismicNewsItems(response.results));
+                      }
+                  });
+    });
+}
+
+function processPrismicNewsItems(items) {
+    return items.map( (item) => ({
+        title: item.getStructuredText('news-panel.title').asText(),
+        image: item.getImage('news-panel.image').url,
+        
+    }) );
+}
+
+//getNewsItemsFromPrismic(console.log);
+
+//--------------------------------------------------------------------------
+
 
 class NewsPanel extends React.Component {
     render() {
         return (
             <div className="news-panel">
             <h2>{this.props.title}</h2>
-            <img src={this.props.img} />
+            <div className="news-img"><img src={this.props.img} /></div>
             <p>{this.props.description}</p>
             </div>
         );
@@ -15,18 +46,19 @@ class NewsPage extends React.Component {
     constructor() {
         super();
         this.state = {
-            newsItems: [],
+            newsItems: [ { title: 1 }, ],
         };
     }
 
     componentWillMount() {
-        //this.getNewsItemsFromPrismic();
+        getNewsItemsFromPrismic( (items) => this.setState({ 
+          newsItems: items }) );
     }
 
     render() {
         const panels = this.state.newsItems.map( (item) =>               
-            <NewsPanel title={item} img="http://placehold.it/350x150" 
-            description="hahahahaha!!" /> );
+            <NewsPanel title={item.title} img={item.image}
+                       description="hahahahaha!!" /> );
         return (
             <div>{panels}</div>
         );
