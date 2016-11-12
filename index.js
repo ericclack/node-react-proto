594319c7850a9f35b1ec4d2d2083d1820a958142
Eric Clack <eric@bright-interactive.com>
2016-11-12 14:35:31 +0000

    Added nodejs

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => (
+    response.render('pages/index')
+));
+
+app.get('/hello', (request, response) => (
+    response.send('Hello Nico!')
+));
+
+app.listen(app.get('port'), () => (
+    console.log('Node app is running on port', app.get('port'))
+));
+
+
