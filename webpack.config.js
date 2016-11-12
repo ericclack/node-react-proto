7171aeec844e58be3abca038e9c993204737b012
Eric Clack <eric@bright-interactive.com>
2016-11-12 16:23:22 +0000

    Added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
     output: {
         path: __dirname,
         filename: '/public/lib/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', 
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
