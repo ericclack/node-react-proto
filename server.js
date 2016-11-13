01ded570390d957567a6d162765d84bd5725523c
Eric Clack <eric@bright-interactive.com>
2016-11-13 18:29:46 +0000

    Changes for Stormpath

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
@@ -9,13 +11,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', (request, response) => (
+app.use(stormpath.init(app, {
+    expand: {
+        customData: true
+    }
+}));
+
+app.get('/', stormpath.getUser, (request, response) => (
     response.render('pages/index')
 ));
 
 app.get('/hello', (request, response) => {
     const name = request.query.name || 'Nico';
     response.send('Hello ' + name + '!')
+});
+
+app.on('stormpath.ready',function(){
+    console.log('Stormpath Ready');
 });
 
 var server = app.listen(app.get('port'), () => (
